073664249b7e6b80da93200f1c9d5514a88bae0e
Thais B <thais_brt@hotmail.com>
2022-04-14 17:18:30 -0300

    nomes imprimindo na tela

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,41 +1,40 @@
-import { getPokemon } from '../../store/actions/PokemonActions'
-import { connect } from 'react-redux';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { getPokemon } from "../../store/actions/PokemonActions";
+import { connect } from "react-redux";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-interface arrayPoke{
-    id: number
-}
+function Home(reducer: any) {
+  const { pokemons, dispatch } = reducer;
+  console.log(pokemons);
+  // const [dataobj, setData] = useState<arrayPoke | null>();
 
-function Home(pokemons:any) {
-    const arrayPokemons = pokemons.pokemons;
-    const {dispatch} = pokemons
-    console.log(arrayPokemons, "olá")
-    const [dataobj, setData] = useState<arrayPoke | null>()
+  // const tryPoke = async (variavel: any) => {
+  //   try {
+  //     const { data } = await axios.get(`${variavel}`);
+  //     console.log(data, "esse aqui");
+  //     // setData(data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
-
-    
   return (
     <div>
-        <button onClick={() => getPokemon(dispatch)}></button>
-    {arrayPokemons.forEach(async (e:any)=>{
-            try {
-                const {data} = await axios.get(`${e.url}`);
-                console.log(data, "esse aqui");
-                setData(data)
-                
-            } catch (error) {
-                console.log(error)
-            }
-            
-        })}
+      <h1>Pokemons</h1>
+      <button onClick={() => getPokemon(dispatch)}>Catch Pokemon</button>
+      {pokemons.map((e: any, index: number) => {
+        return (
+          <div key={index}>
+            <p>{e.name}</p>
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
 
-const mapStateToProps = (state:any) => ({
-    pokemons: state.pokemonReducer.pokemons,
-  });
+const mapStateToProps = (state: any) => ({
+  pokemons: state.pokemonReducer.pokemons,
+});
 
-
-export default connect(mapStateToProps)<any>(Home)
+export default connect(mapStateToProps)<any>(Home);
