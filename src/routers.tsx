c7a05cacb2f89b16962b37a75d2d080ad6d78b63
Nathalia Duarte <duartenatha@gmail.com>
2022-04-15 17:02:39 -0300

    organizada as rotas

--- src/routers.tsx
+++ src/routers.tsx
@@ -1,7 +1,6 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import Detail from './pages/detail/Detail'
 import Home from './pages/home/Home'
-import Search2 from './pages/home/Searching'
 
 
 function Routers() {
@@ -12,7 +11,6 @@ function Routers() {
             <Route path="/detail" element={ <Detail /> } >
                 <Route path=":id" element={ <Detail /> } />
             </Route>
-            <Route path="/search" element={ <Search2 /> } />
         </Routes>
     </BrowserRouter>
   )
